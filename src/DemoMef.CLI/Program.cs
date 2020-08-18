9a65158bf89562ac1cd4eab7065701badc58611c
Xavier Dervaux <dex@smart.coop>
2020-08-18 16:15:54 +0200

    Initial Commit

---
+++ src/DemoMef.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoMef.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
