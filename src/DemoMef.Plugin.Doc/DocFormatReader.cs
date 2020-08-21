7c23f1d4526619f9ebd0d3984384ded42666df37
Xavier Dervaux <dex@smart.coop>
2020-08-21 11:12:21 +0200

    Add Doc support

---
+++ src/DemoMef.Plugin.Doc/DocFormatReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using DemoMef.Core;
+using System.ComponentModel.Composition;
+using SautinSoft.Document;
+
+namespace DemoMef.Plugin.Doc
+{
+    [Export(typeof(IFormatReader))]
+    public class DocFormatReader : IFormatReader
+    {
+        public string Format => ".docx";
+
+        public string Read(string path)
+        {
+            var output = "";
+
+            var dc = DocumentCore.Load(path);
+            var list = new List<string>();
+            foreach (var run in dc.GetChildElements(true, ElementType.Run))
+            {
+                output += run.Content.ToString();
+            }
+            return output;
+        }
+    }
+}
